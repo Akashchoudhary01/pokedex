0ac9d9d32497acd145a0a0a6eee3c0a0d109b57b
Akash Choudhary <akashkrchoudhary007@gmail.com>
2025-08-22 22:19:57 +0530

    --

--- Pokedex/src/Components/PokemonList/PokemonList.jsx
+++ Pokedex/src/Components/PokemonList/PokemonList.jsx
@@ -1,50 +1,25 @@
 import Pokemon from "../Pokemon/Pokemon";
 import usePokemonList from "../../Hooks/usePokemonList";
 
-export default function PokemonList() {
-  const { pokemonListState, setpokemonListState } = usePokemonList(
-    `https://pokeapi.co/api/v2/pokemon`,
-    false
+export default function PokemonList({ searchTerm }) {
+  const { pokemonListState } = usePokemonList(`https://pokeapi.co/api/v2/pokemon`);
+
+  if (!pokemonListState || !pokemonListState.pokemonList) {
+    return <p className="text-center text-white">Loading...</p>;
+  }
+
+  // Filter based on search term
+  const filteredList = pokemonListState.pokemonList.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   return (
-    <div className="mx-auto mt-10 p-6 font-poppins">
-      {/* Pokémon list */}
-      <div className="flex flex-wrap justify-center gap-6">
-        {pokemonListState.isLoading
-          ? "Loading ....."
-          : pokemonListState.pokemonList.map((p) => (
-              <Pokemon key={p.id} name={p.name} image={p.image} id={p.id} />
-            ))}
-      </div>
-
-      {/* Pagination buttons */}
-      <div className="flex justify-center gap-6 mt-8">
-        <button
-          onClick={() =>
-            setpokemonListState((prev) => ({
-              ...prev,
-              POKEDEX_URL: prev.prevUrl,
-            }))
-          }
-          className="px-4 py-2 text-black rounded-lg shadow outline-1 outline-black"
-          disabled={!pokemonListState.prevUrl}
-        >
-          Previous
-        </button>
-        <button
-          onClick={() =>
-            setpokemonListState((prev) => ({
-              ...prev,
-              POKEDEX_URL: prev.nextUrl,
-            }))
-          }
-          className="px-4 py-2 text-black rounded-lg shadow outline-1 outline-black"
-          disabled={!pokemonListState.nextUrl}
-        >
-          Next
-        </button>
-      </div>
+    <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-4">
+      {filteredList.length > 0 ? (
+        filteredList.map((p) => <Pokemon key={p.id} pokemon={p} />)
+      ) : (
+        <p className="col-span-full text-center text-gray-300">No Pokémon found</p>
+      )}
     </div>
   );
 }
